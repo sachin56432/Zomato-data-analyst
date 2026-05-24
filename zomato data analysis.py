faedb2f1edde11254390eed412c0ed54ef91dc7d
Sachin Sharma <sachinsharma56432@gmail.com>
2026-05-24 23:28:24 +0530

    Initial commit

---
+++ zomato data analysis.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataframe = pd.read_csv(r"C:\Users\Sourav Sharma\OneDrive\Documents\Desktop\python\Zomato-data-.csv")
+print(dataframe.head())
+
+def handlerate(value):
+    value= str(value).split('/')
+    value=value[0];
+    return float(value)
+
+dataframe['rate']= dataframe['rate'].apply(handlerate)
+print(dataframe.head())
+
+dataframe.info()
+
+print(dataframe.isnull().sum())
+
+sns.countplot(x=dataframe['listed_in(type)'])
+plt.xlabel('Types of Restaurant')
+
+
+grouped_data= dataframe.groupby('listed_in(type)')['votes'].sum()
+result=pd.DataFrame({'votes': grouped_data})
+plt.plot(result,c='Green',marker='o')
+plt.xlabel('Type of restaurantt')
+plt.ylabel('Votes')
+plt.show()
